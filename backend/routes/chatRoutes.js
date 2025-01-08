7c016d69915107be60baa6a80c2d8f37ef12cae3
Harshana Wijerathne <harshanawijerathne08@gmail.com>
2025-01-08 07:27:16 +0530

    Fetch user All chats

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { accessChat } = require("../controllers/chatController");
+const { accessChat, fetchChats } = require("../controllers/chatController");
 const { protect } = require("../Middleware/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChats);
+router.route("/").get(protect, fetchChats);
 // router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
